1ea0069bf41a4be0b6d0d132764de871214f731d
haydar-99 <haydaralsaadi33@gmail.com>
2022-05-08 22:43:32 +0200

    sucessful verification + hasing, no database is integrated yet

--- router/router.js
+++ router/router.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router()
 const bcrypt = require("bcrypt");
+const { route } = require("express/lib/application");
+const { hash } = require("bcrypt");
 
 const users = [];
+//create a user 
 router.post("/signup", async(req,res) =>{
+    const user = users.find(user => user.email == req.body.email)
+    if (user) {
+        res.send("this email is already in use")
+    }
     try {
         req.body.password =   await bcrypt.hash(req.body.password, 10)
         users.push(req.body)
-        console.log(req.body)
         
     } catch (error) {
         console.log(error)
@@ -17,4 +23,21 @@ router.post("/signup", async(req,res) =>{
     res.json(req.body )
 }) 
 
+router.post("/login", async(req,res)=>{
+    const user = users.find(user => user.email == req.body.email)
+    if (user) {
+        try {
+            if(await bcrypt.compare(req.body.password, user.password)){ res.send("succefully logged")}
+            else{res.send("wrong password")}
+        } catch (error) {
+            console.log(error)
+        }
+        
+    }
+    else{
+        res.send("nothing is found ")
+    }
+  
+
+})
 module.exports = router
